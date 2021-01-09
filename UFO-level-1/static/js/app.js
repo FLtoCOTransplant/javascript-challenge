c01bf626cf798389d80fc6fc37ee6b05b634f6f1
FLtoCOTransplant <67385063+FLtoCOTransplant@users.noreply.github.com>
2021-01-08 23:05:49 -0700

    working on table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,4 +4,23 @@ var tableData = data;
 // YOUR CODE HERE!
 
 // Create a variable for the table columns
-var tblColumns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+var tableColumns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+
+// Create HTML object references
+var tablebody = d3.select("tablebody");
+var searchDate = d3.select("#searchDate");
+var btnSearch = d3.select("#btnSearch");
+var btnReset = d3.select("#btnReset");
+
+// Define a function that builds the table
+var loadTableRows = (xData) => { 
+	// Delete the previously loaded table rows (if there were any)
+    tablebody.html("");
+	
+	// Loop through "whichData" and add new table row for each row 
+	whichData.forEach(dataRow => { var tblRow = tbody.append("tr");  
+		
+		// Loop through each column and add it as a new table cell
+		tblColumns.forEach(column => tblRow.append("td").text(dataRow[column]))
+	});
+}
